fe19ddbff6e1a95b1956364b87eca3a17aacc6bb
Jhon Angulo <jhon.angulo@jikkosoft.com>
2022-01-04 14:29:24 -0500

    add header, footer and other components

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="imagen de usuario">
+    </div>
+    <div class="Profile-info">
+      <h2>@jman</h2>
+      <span>Jhon Angulo</span>
+    </div>
+  </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,17 @@
+<div class="Stories">
+  <div class="Stories-container">
+    <div class="Stories-head">
+      <h2>Historias</h2>
+      <span>Ver todas</span>
+    </div>
+    <div class="Stories-items">
+      <div class="Stories-item">
+        <div class="Stories-item-box">
+          <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="imagen de usuario">
+        </div>
+        <h2>@jhonangulo</h2>
+        <span>10 horas antes</span>
+      </div>
+    </div>
+  </div>
+</div>
